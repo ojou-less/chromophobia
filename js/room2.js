ba2621b85c4846bd433519c86397f806c859232f
ojou-less <mark.fischer0404@gmail.com>
2023-07-12 18:05:45 +0200

    gefickt

--- js/room2.js
+++ js/room2.js
@@ -1,42 +1,11 @@
-
-const SCREENWIDTH = 800;
-const SCREENHEIGHT = 608;
-
-let gameScene = new Phaser.Scene('Game');
-
-let config = {
-
-    type: Phaser.AUTO,
-    width: 800,
-    height: 608,
-    scene: gameScene,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-            debug: false
-        }
-    },
-
-    scene: [gameScene, room1]
-};
-
-let player;
-//let enemies = [];
-
-let bombs;
-
-let cursors;
-let gameOver = false;
-let gameoverText;
-let roomText;
+let room1 = new Phaser.Scene('room1');
+let room1Text;
+let gameoverTextRoom1;
 
 
-let game = new Phaser.Game(config);
-
-gameScene.preload = function()
+room1.preload = function()
 {
-
+    
     // -----------------------------------------------------------------------------------
     // Loading Audio Assests
     this.load.audio("gameover", "assets/audios/dyingsound.mp3");
@@ -47,14 +16,12 @@ gameScene.preload = function()
 
     // -----------------------------------------------------------------------------------
     // Loading Image Assests
-    this.load.image("tiles1", "assets/images/forest_.png");
-    this.load.image("tiles1_resources", "assets/images/forest_resources.png");
-    this.load.tilemapTiledJSON("map1", "assets/json/chromophobia_main_room.json");
+    this.load.image("tiles1", "assets/images/swamp_.png");
+    this.load.image("tiles1_resources", "assets/images/swamp_resources.png");
+    this.load.tilemapTiledJSON("map1", "assets/json/chromophobia_room2.json");
 
     this.load.image('star', 'assets/images/star.png');
     this.load.image('bomb', 'assets/images/bomb.png');
-
-
 
     // -----------------------------------------------------------------------------------
     // Loading Player Assests
@@ -65,27 +32,28 @@ gameScene.preload = function()
     // Loading Player Assests
     this.load.spritesheet('idleEnemy', 'assets/images/IdleEnemy.png', { frameWidth: 21, frameHeight: 30 });
     this.load.spritesheet('walkingEnemy', 'assets/images/WalkingEnemy.png', { frameWidth: 21, frameHeight: 30 });
-
-    console.log("preload");
 }
 
-gameScene.create = function()
+    
+room1.create = function()
 {
-    console.log("create");
+    
     const map = this.make.tilemap({
         key: "map1",
         tileWidth: 16,
         tileHeight: 16
     });
 
-    const tileset = map.addTilesetImage("forest_", "tiles1");
-    const treetiles = map.addTilesetImage("forest_ [resources]", "tiles1_resources");
+    const tileset = map.addTilesetImage("swamp_", "tiles1");
+    const treetiles = map.addTilesetImage("swamp_ [resources]", "tiles1_resources");
     const bglayer = map.createLayer("Background", tileset, 0, 0);
-    const treelayer = map.createLayer("Trees", treetiles, 0, 0);
+    const treelayer = map.createLayer("Obstacles", treetiles, 0, 0);
     const portallayer = map.createLayer("Portal", tileset, 0, 0);
+    
 
     // -----------------------------------------------------------------------------------
     // Player Animations
+    /*
     this.anims.create({
         key: 'main-walk-front',
         frames: this.anims.generateFrameNumbers('walkingMain', {frames:[0, 3, 6, 9]}),
@@ -166,86 +134,70 @@ gameScene.create = function()
         frames: this.anims.generateFrameNumbers('idleEnemy', {frames:[2, 5]} ),
         frameRate: 2
     });
+    */
 
     cursors = this.input.keyboard.createCursorKeys();
-    player = new MainCharacter(gameScene, 100, 450, 200, 400, new Bullets(gameScene, 400, 200, 50, 'white'));
-    this.enemies = [new Enemy(gameScene, player.getEntity(), 100, 100, 70, 200, 200, 'blue', new Bullets(gameScene, 200, 700, 40, 'red'))];
-    this.enemies.push(new Enemy(gameScene, player.getEntity(), 400, 400, 70, 200, 200, 'blue', new Bullets(gameScene, 200, 700, 40, 'red')));
+    player = new MainCharacter(this, 100, 450, 200, 400, new Bullets(this, 400, 200, 50, 'white'));
+    enemies = new Enemy(this, player.getEntity(), 100, 100, 100, 300, 200, 'blue', new Bullets(this, 200, 500, 50, 'red'));
+    //console.log(player);
+
 
     this.physics.add.collider(player.getEntity(), bglayer);
     this.physics.add.collider(player.getEntity(), treelayer);
-    this.physics.add.collider(player.getEntity(), portallayer, enterRoom2, null, this);
+    // treelayer.setCollisionBetween(214, 228);
+    this.physics.add.collider(player.getEntity(), treelayer, sceneCompleted, null, this);
 
     treelayer.setCollisionByProperty({collides:true});
-
-    function enterRoom2() {
-        console.log("hallo")
-        room1.preload();
-        this.scene.start(room1);
-    };
-
-    
-
-    for(let i = 0; i < this.enemies.length; i++)
-    {
-        console.log(this.enemies[i]);
-        for(let j = 0; j < this.enemies.length; j++)
-        {
-            this.physics.add.collider(this.enemies[i].getEntity(), this.enemies[j].getEntity());
-        }
-
-        this.physics.add.collider(player.getEntity(), this.enemies[i].getEntity());
-        this.physics.add.overlap(player.bullet, this.enemies[i].getEntity(), test1, null, this);
-        this.physics.add.overlap(this.enemies[i].bullet, player.getEntity(), test1, null, this);
-        this.physics.add.collider(this.enemies[i].getEntity(), treelayer);
+   
+    function sceneCompleted(){
+        console.log("portal betreten!");
     }
+
+
+    this.input.keyboard.on("keydown-A", () => {
+        gameScene.preload();
+
+        this.scene.start(gameScene);
+    });
+
+    this.physics.add.collider(player.getEntity(), enemies.getEntity());
+
+    this.physics.add.overlap(player.bullet, enemies.getEntity(), test2, null, this);
+    this.physics.add.overlap(enemies.bullet, player.getEntity(), test2, null, this);
     
-    let background = this.sound.add("background", {volume: 0.01});
-    background.play();
 
-    gameoverText = gameScene.add.text(400, 300, "Game Over!\nPlease click into the field to restart", {fontSize: "30px", fill: "#000"});
-    roomText = gameScene.add.text(16, 16, "Main Room", {fontSize: "16px", fill: "#000"});
-    gameoverText.setOrigin(0.5);
-    gameoverText.setVisible(false);
+    room1Text = room1.add.text(16, 16, "Room1 Room", {fontSize: "16px", fill: "#000"});
+    gameoverTextRoom1 = room1.add.text(400, 300, "Game Over!\nPlease click into the field to restart", {fontSize: "30px", fill: "#000"});
+    gameoverTextRoom1.setOrigin(0.5);
+    gameoverTextRoom1.setVisible(false);
 }
 
-function tst()
-{
-    console.log("ouch");
-}
-
-function test1(character, bullet)
+function test2(character, bullet)
 {
     if(bullet.active)
     {
+        console.log("Scene 2");
         character.hit(bullet.damage, bullet.color);
         let gotshot = this.sound.add("hitsound", {volume: 0.01}, { loop: false});
         gotshot.play();
-        console.log(character.health);
-
-        if(character.dead())
+        if(enemies.getEntity() === character)
         {
-            bullet.setVisible(false);
-            for(let i = 0; i < this.enemies.length; i++)
-            {
-                if(this.enemies[i].getEntity() === character)
-                {
-                    this.enemies[i].entity = null;
-                    this.enemies.splice(i,1);
-                    console.log(this.enemies);
-                }
-            }
+            console.log("nice");
         }
+        console.log(enemies);
+        console.log(character);
+        character.dead();
     }
     bullet.setActive(false);
     bullet.setVisible(false);
+    console.log(character.health);
+
 }
 
-gameScene.update = function()
+
+    
+room1.update = function() 
 {
     player.movement();
-    for(let i = 0; i < this.enemies.length; i++)
-    {
-        this.enemies[i].update();
-    }
+    enemies.update();
 }
